d5b8bebc3ffa022853595d8cd6454287c6bf33ec
Nikita Morkovkin <nikitamorkovkinwork@gmail.com>
2025-12-05 18:55:06 +0500

    fix: Fix validation pipe and cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +9,7 @@ async function bootstrap() {
     origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', ''],
   });
 
   app.useGlobalPipes(new ValidationPipe());
